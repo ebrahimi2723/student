52f596341a4ffd0a361678605504f22e441bb1cb
ebrahimi2723 <ebrahimi2723>
2022-04-15 23:32:29 +0430

    lets start

---
+++ src/main/java/com/ebrahimi2723/MainApp.java
@@ -0,0 +1,10 @@
+package com.ebrahimi2723;
+
+
+import com.ebrahimi2723.io.Io;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println(Io.input());
+    }
+}
---
+++ src/main/java/com/ebrahimi2723/io/Io.java
@@ -0,0 +1,10 @@
+package com.ebrahimi2723.io;
+
+import java.util.Scanner;
+
+public  class Io {
+    public static String input(){
+       Scanner scanner = new Scanner(System.in);
+      return scanner.next();
+    }
+}
